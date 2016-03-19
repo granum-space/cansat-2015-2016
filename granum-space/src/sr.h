b5438f7841edb33f4dc39c6b389f381b7457781a
khrisanovaAn <k01hrisanova@mail.ru>
2016-03-19 18:09:12 +0300

    Добавлены burn.h и sr.h

---
+++ granum-space/src/sr.h
@@ -0,0 +1,15 @@
+/*
+ * sr.h
+ *
+ *  Created on: 19 марта 2016 г.
+ *      Author: developer
+ */
+
+#ifndef SR_H_
+#define SR_H_
+#include <stdio.h>
+
+void sr_init();
+uint16_t sr_getrezistance(int pair);
+
+#endif /* SR_H_ */
