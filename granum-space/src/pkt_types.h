dc54c3774145d5d7630f0adf0b9b9c1eb022627c
Kirs67 <kirilel200267@gmail.com>
2016-03-29 19:29:00 +0300

    Добавлен pkt_types.h (там задефайнены типы пакетов)

---
+++ granum-space/src/pkt_types.h
@@ -0,0 +1,24 @@
+/*
+ * pkt_types.h
+ *
+ *  Created on: 29 марта 2016 г.
+ *      Author: developer
+ */
+
+#ifndef PKT_TYPES_H_
+#define PKT_TYPES_H_
+typedef struct {
+	uint16_t beacon; //must be 0xFFFF or 65535
+	uint16_t number;
+	uint16_t time, time_part;
+	uint16_t temperature1, temperature2,
+		pressure,
+		humidity,
+		O2, CO2,
+		rezistance12, rezistance23, rezistance12;
+	uint8_t legs, parachute;
+	uint16_t cntrl;
+} dtpkt_t;
+
+
+#endif /* PKT_TYPES_H_ */
