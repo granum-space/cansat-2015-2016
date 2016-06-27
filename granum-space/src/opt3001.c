3c21ba705f9ef1c0c975424cedb56948bba5d029
vasilly.prokopyev <vasilly.prokopyev@gmail.com>
2016-06-27 20:21:26 +0300

    подправил опт осталось сделать функцию на измерение

--- granum-space/src/opt3001.c
+++ granum-space/src/opt3001.c
@@ -12,54 +12,110 @@
 #include "i2c.h"
 
 #define OPT3001_ADDR 0x10  // АДРЕСС ДАТЧИКА
-#define ADDR_REG 01 // адрес регистра в котором лежит конфигурация в hex
-#define ADDR_RES 00 //адрес регистра из которого мы хотим прочитать результат в hex
-#define BYN_OPT 0000010000000000
+#define ADDR_REG 0x01 // адрес регистра в котором лежит конфигурация в hex
+#define ADDR_RES 0x00 //адрес регистра из которого мы хотим прочитать результат в hex
+#define BYN_OPT (1 << 10)
 
-uint16_t config_opt;
-uint16_t result;
 
-int OPT_read(uint8_t ADDR_read){
+// биты конфигурационного регистра:
+// ========================================================
+// Биты управления диапазоном измерения OPT
+#define OPT_CFG_RN0 12
+#define OPT_CFG_RN1 13
+#define OPT_CFG_RN2 14
+#define OPT_CFG_RN3 15
+// комбинации указаны в даташите, таблица 9, на странице 20
+
+// время измерения.
+#define OPT_CFG_CT 11
+// 0 => 100ms, 1 => 800ms
+
+// режим измерения
+#define OPT_CFG_M0 10
+#define OPT_CFG_M1 9
+// 00 => режим ожидания; 01 => одиночное измерение; 10 или 11 => непрерывные измерения
+
+// флаг переполнения
+#define OPT_CFG_OVF 8
+// когда в этом флаге 1 - это означает, что прибор "зашкалило" и действительное значение яркости больше чем
+// зафиксированно
+
+// флаг готовности результата
+#define OPT_CFG_CRF 7
+// читается как единица, когда преобразование заверешено. Сбрасывается повторным чтением/записью
+// конфигурационного регистра.
+
+// признак перехода за верхний предел освещенности
+#define OPT_CFG_FH 6
+// этот бит устанавливается в единичку, когда значение освещенности превышает значение
+// заданное в регистре High Limit (0x03). Возможна генерация сигнала прерывания по этому событию
+
+// признак перехода за нижний предел освещенности
+#define OPT_CFG_FL 5
+// этот бит устанавливается в единичку, когда значение освещенности становится меньше, чем значение
+// заданное в регистре Low Limit (0x02). Возможна генерация сигнала прерывания по этому событию
+
+// Режим "защелкивания прерывания"
+#define OPT_CFG_L 4
+// При единичке в этом бите, состояние прерывания наступает после N последовательных измерений,
+// которые дают значение большее чем в High Limit регистре или меньшее чем Low Limit.
+// при этом состояние прерывания сбрасывается только при чтении конфигурационного регистра
+//
+// при нуле в этом бите устройство входит в состояние прерывания после N последовательных измерений,
+// которые дают значение больше, чем в High Limit регистре. Выход из состояния прерывания
+// происходит после N измерений, которые дают значение меньшее чем в Low Limit регистре.
+// чтение конфигурационного регистра флаг прерывания не снимает
+
+// полярность сигнала прерывания
+#define OPT_CFG_POL 3
+// POL == 0. Состояние прерывания обозначается нулём на INT пине OPT3001
+// POL == 1. Состояние прерывания обозначается единичкой на INT пине OPT3001
+
+// маскирование экспоненты результата измерения.
+#define OPT_CFG_ME 2
+// управляет представлением результата измерения при заданных в ручную пределах измерения
+
+
+// Управляет количеством измерений, по которым осуществляется переход в состояние прерывания
+#define OPT_CFG_FC1 1
+#define OPT_CFG_FC0 0
+// 00 -> Одно измерение
+// 01 -> Два измерения
+// 10 -> Четыре измерения
+// 11 -> Восемь измерений
+
+int OPT_read(uint8_t ADDR_read, uint16_t * value){
 
 	int flag = 0;
+
 	flag = i2c_start();
 	if(flag != 0)
 		return flag;
+
 	flag = i2c_send_slaw(OPT3001_ADDR, false);
 	if(flag != 0){
 		i2c_stop();
 		return flag;
 	}
+
 	uint8_t target_register_addr = ADDR_read;
 	flag = i2c_write(&target_register_addr, 1);
 	if(flag != 0 ){
 		i2c_stop();
 		return flag;
 	}
+
 	i2c_start();
 	flag = i2c_send_slaw(OPT3001_ADDR, true);
 	    if (flag != 0){
 	        i2c_stop();
 	        return flag;
-	    }
-	 if(ADDR_read == ADDR_REG){
-		 flag = i2c_read(&config_opt, 2, true);
-		 if (flag != 0) {
-	        i2c_stop();
-	        return flag;
-		 	 }
-		 i2c_stop();
-		 return flag;
 	 }
-	 else{
-		 flag = i2c_read(&result, 2, true);
-	 	 if (flag != 0) {
-	 		 i2c_stop();
-	 	     return flag;
-	 	 }
-	 	 i2c_stop();
-	 	 return flag;
-	 }
+
+	 flag = i2c_read(&value, 2, true);
+
+	 i2c_stop();
+	 return flag;
 }
 
 uint16_t OPT_RESULT(){
@@ -67,16 +123,19 @@ uint16_t OPT_RESULT(){
 	return result;
 }
 
-int OPT_write(uint8_t ADDR_write){
+int OPT_write(uint8_t ADDR_write, uint16_t * value){
 	int flag = 0;
+
 	flag = i2c_start();
 	if(flag != 0)
 		return flag;
+
 	flag = i2c_send_slaw(OPT3001_ADDR, false);
 	if(flag != 0){
 		i2c_stop();
 		return flag;
 	}
+
 	uint8_t target_register_addr = ADDR_write;
 	flag = i2c_write(&target_register_addr, 1);
 	if (flag != 0)
@@ -84,26 +143,44 @@ int OPT_write(uint8_t ADDR_write){
 		i2c_stop();
 	    return flag;
 	}
-	if(ADDR_write == ADDR_REG) {
-		flag = i2c_write(&config_opt, 2);
-	}
+	flag = i2c_write(value, 2);
 	i2c_stop();
 	return flag;
 }
 
 void OPT_init(){
 
-	OPT_read(ADDR_REG);
+	uint16_t cfg_reg =
+	        // автоматическое определение пределов измерения
+	        (0 << OPT_CFG_RN0) | (0 << OPT_CFG_RN1) | (1 << OPT_CFG_RN2) | (1 << OPT_CFG_RN3) |
+	        // длительность измерений на 100мс
+	        (0 << OPT_CFG_CT) |
+	        // режим ожидания
+	        (0 << OPT_CFG_M0) | (0 << OPT_CFG_M1) |
+	        // флаг переполнения (OVF) не трогаем
+	        // флаг заверешния преобразования (CRF) не трогаем
+	        // флаги прерываний (FH и FL) не трогаем
+	        // режим прерывания ставим на защелкиваемый (состяние удерживается до чтения из регистра конфигурации)
+	        (1 << OPT_CFG_L) |
+	        // полярность прерывания - единица означает активное прерывание
+	        (1 << OPT_CFG_POL) |
+	        // не маскируем экспоненциальную часть результата
+	        (0 << OPT_CFG_ME) |
+	        // ставим по максимуму (8) измерений превыщающих предел для перехода в прерывание
+	        (1 << OPT_CFG_FC0) | (1 << OPT_CFG_FC1)
+	    ;
+
 	// меняем конфигурацию
-	// config_opt = 000110111010011; например
-	OPT_write(ADDR_REG);  // отправлеям новую конфу на запись
+	OPT_write(ADDR_REG, &cfg_reg);  // отправлеям новую конфу на запись
 }
 
-uint8_t OPT_start(){
-		OPT_read(ADDR_REG);
-		if(config_opt & BYN_OPT)
-			return 1;
-		else return 0;
+uint8_t OPT_check(){
+	uint16_t cfg_reg;
+	OPT_read(ADDR_REG, &cfg_reg);
+	if(cfg_reg & BYN_OPT)
+		return 1;
+	else
+		return 0;
 }
 
 
